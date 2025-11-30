fb75a177d2a2b85c900f3c993bdeed06e1fd3714
Kamoteshake <kamoteshake@gmail.com>
2025-11-29 22:37:44 -0600

    fix: sample test

--- client/src/pages/Home/Home.spec.tsx
+++ client/src/pages/Home/Home.spec.tsx
@@ -1,17 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import { ApolloProvider } from '@apollo/client/react';
 import { AppThemeProvider } from 'theme/ThemeProvider';
-import { apolloClient } from 'src/apolloClient';
+import { AuthenticatedApolloProvider } from 'apollo/AuthenticatedApolloProvider';
 import { Home } from '.';
 
 describe('Home page', () => {
   it('renders the main heading', () => {
     render(
-      <ApolloProvider client={apolloClient}>
+      <AuthenticatedApolloProvider>
         <AppThemeProvider>
           <Home />
         </AppThemeProvider>
-      </ApolloProvider>,
+      </AuthenticatedApolloProvider>,
     );
 
     expect(screen.getByRole('heading', { name: /full stack template/i })).toBeInTheDocument();
